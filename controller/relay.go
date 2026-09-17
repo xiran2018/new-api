b3dc74bfb205e66c56f0dfb616b09098d8ab6f1f
xiran2018 <540692237@qq.com>
2026-09-17 12:25:34 +0800

    fix: preserve billing request dto import after merge

--- controller/relay.go
+++ controller/relay.go
@@ -22,6 +22,7 @@ import (
 	relaycommon "github.com/QuantumNous/new-api/relay/common"
 	relayconstant "github.com/QuantumNous/new-api/relay/constant"
 	"github.com/QuantumNous/new-api/relay/helper"
+	"github.com/QuantumNous/new-api/relaykit/dto"
 	"github.com/QuantumNous/new-api/relaykit/types"
 	"github.com/QuantumNous/new-api/service"
 	"github.com/QuantumNous/new-api/setting/operation_setting"
